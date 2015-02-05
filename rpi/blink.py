aa07fdc5d19100983741f064722baaca3e51942b
pony-dev <pony-dev@users.noreply.github.com>
2015-02-05 20:24:47 +0100

    Update blink.py

--- rpi/blink.py
+++ rpi/blink.py
@@ -3,12 +3,15 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-while True:
-  GPIO.output(12, False)
-  sleep(1)
-  GPIO.output(12, True)
-  sleep(1)
+try:
+   while True:
+      GPIO.output(12, False)
+      sleep(1)
+      GPIO.output(12, True)
+      sleep(1)
+
+finally:
+   GPIO.cleanup()
