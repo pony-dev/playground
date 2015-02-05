cf12257b19845c9af6df7e79181f897a6dc12d15
pony-dev <pony-dev@users.noreply.github.com>
2015-02-05 22:46:09 +0100

    Update: blink.py

--- rpi/blink.py
+++ rpi/blink.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 
+# Importing librarys
 from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+# Setting up GPIO pins
+GPIO.setmode(GPIO.BOARD)	# 
+GPIO.setup(12, GPIO.OUT)	# set pin 12 as output
 
 try:
-   while True:
-      GPIO.output(12, False)
-      sleep(1)
-      GPIO.output(12, True)
-      sleep(1)
+   while True:			# loops until interrupt (Ctrl+C)
+      GPIO.output(12, False)	# set pin 12 low
+      sleep(1)			# wait 1s
+      GPIO.output(12, True)	# set pin 12 high
+      sleep(1)			# wait 1s
 
 finally:
    GPIO.cleanup()
